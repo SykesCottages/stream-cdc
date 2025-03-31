1ef5902abfc6bf577b0db9f8df551a0fce7e0d91
llandmayer <leandro.landmayer@gmail.com>
2025-03-31 16:59:31 +0100

    MOUR-2743 - Refactorying registry so it's possible to split the stream and datasource packages into individual modules; Adding validation for mysql config settings that need to be enabled in order for the script to run

--- stream_cdc/streams/__init__.py
+++ stream_cdc/streams/__init__.py
@@ -1,1 +1,12 @@
+from stream_cdc.streams.base import Stream
+from stream_cdc.streams.factory import StreamFactory
+from stream_cdc.streams.sqs import SQS
 
+# Register the SQS stream with the factory
+StreamFactory.register_stream('sqs', SQS)
+
+__all__ = [
+    'Stream',
+    'StreamFactory',
+    'SQS'
+]
