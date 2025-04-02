13d1ae0bc77be0c0f583dc2f0bdfe63f59bfea1c
llandmayer <leandro.landmayer@gmail.com>
2025-04-02 09:54:38 +0100

    MOUR-2751 - Added dynamodb localstack;Init state_manager

---
+++ stream_cdc/state_manager/state_manager.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class StateManager(ABC):
+
+    @abstractmethod
+    def store(self):
+        pass
+
+
+    @abstractmethod
+    def read(self):
+        pass
